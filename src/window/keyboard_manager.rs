338662c5d9d60bde64430e4df492b79d08329d4d
Lukas Nagel <lukasngl@murena.io>
2023-03-06 14:12:39 +0100

    infer modifier state from key events instead of winit

--- src/window/keyboard_manager.rs
+++ src/window/keyboard_manager.rs
@@ -1,40 +1,26 @@
 use crate::{
     bridge::{SerialCommand, UiCommand},
     event_aggregator::EVENT_AGGREGATOR,
-    settings::SETTINGS,
-    window::KeyboardSettings,
 };
 use glutin::{
     event::{ElementState, Event, KeyEvent, WindowEvent},
-    keyboard::{Key, Key::Dead},
+    keyboard::Key,
     platform::modifier_supplement::KeyEventExtModifierSupplement,
 };
 
-enum InputEvent {
-    KeyEvent(KeyEvent),
-    ImeInput(String),
-}
+#[derive(Debug, Clone, Default, PartialEq)]
 pub struct KeyboardManager {
     shift: bool,
     ctrl: bool,
     alt: bool,
-    prev_dead_key: Option<char>,
     logo: bool,
-    ignore_input_this_frame: bool,
-    queued_input_events: Vec<InputEvent>,
+    prev_dead_key: Option<char>,
+    ignore_input: bool,
 }
 
 impl KeyboardManager {
     pub fn new() -> KeyboardManager {
-        KeyboardManager {
-            shift: false,
-            ctrl: false,
-            alt: false,
-            prev_dead_key: None,
-            logo: false,
-            ignore_input_this_frame: false,
-            queued_input_events: Vec::new(),
-        }
+        KeyboardManager::default()
     }
 
     pub fn handle_event(&mut self, event: &Event<()>) {
@@ -45,7 +31,10 @@ impl KeyboardManager {
             } => {
                 // When window is just focused or lost it's focus, ignore keyboard events
                 // that were submitted this frame
-                self.ignore_input_this_frame = true;
+                self.ignore_input = true;
+            }
+            Event::MainEventsCleared => {
+                self.ignore_input = false;
             }
             Event::WindowEvent {
                 event:
@@ -53,78 +42,42 @@ impl KeyboardManager {
                         event: key_event, ..
                     },
                 ..
-            } => {
-                // Store the event so that we can ignore it properly if the window was just
-                // focused.
-                self.queued_input_events
-                    .push(InputEvent::KeyEvent(key_event.clone()));
+            } if !self.ignore_input => {
+                use ElementState::Pressed;
+
+                match key_event.logical_key {
+                    // Update Modifiers
+                    Key::Alt => self.alt = key_event.state == Pressed,
+                    Key::Control => self.ctrl = key_event.state == Pressed,
+                    Key::Shift => self.shift = key_event.state == Pressed,
+                    Key::Super => self.logo = key_event.state == Pressed,
+
+                    // handle Deadkey
+                    Key::Dead(dead_key) => {
+                        self.prev_dead_key = dead_key;
+                    }
+
+                    // handle Key Presses
+                    _ if key_event.state == Pressed => {
+                        let _ = self
+                            .maybe_get_keybinding(key_event)
+                            .map(SerialCommand::Keyboard)
+                            .map(UiCommand::Serial)
+                            .map(|cmd| EVENT_AGGREGATOR.send(cmd));
+                    }
+                    _ => (),
+                }
             }
             Event::WindowEvent {
                 event: WindowEvent::ReceivedImeText(string),
                 ..
             } => {
-                self.queued_input_events
-                    .push(InputEvent::ImeInput(string.to_string()));
-            }
-            Event::WindowEvent {
-                event: WindowEvent::ModifiersChanged(modifiers),
-                ..
-            } => {
-                // Record the modifier states so that we can properly add them to the keybinding
-                // text
-                self.shift = modifiers.shift_key();
-                self.ctrl = modifiers.control_key();
-                self.alt = modifiers.alt_key();
-                self.logo = modifiers.super_key();
-            }
-            Event::MainEventsCleared => {
-                // If the window wasn't just focused.
-                if !self.should_ignore_input() {
-                    // And we have a keyboard event this frame
-                    for input_event in self.queued_input_events.iter() {
-                        let mut next_dead_key = self.prev_dead_key;
-                        match input_event {
-                            InputEvent::KeyEvent(key_event) => {
-                                // And a key was pressed
-                                if key_event.state == ElementState::Pressed {
-                                    if let Some(keybinding) = self.maybe_get_keybinding(key_event) {
-                                        EVENT_AGGREGATOR.send(UiCommand::Serial(
-                                            SerialCommand::Keyboard(keybinding),
-                                        ));
-                                    }
-                                    next_dead_key = None;
-                                } else if key_event.state == ElementState::Released {
-                                    // dead key detect here
-                                    if let Dead(dead_key) = key_event.logical_key {
-                                        // should wait for the next input text_with_all_modifiers, and ignore the next ime.
-                                        next_dead_key = dead_key;
-                                    }
-                                }
-                            }
-                            InputEvent::ImeInput(raw_input) => {
-                                if self.prev_dead_key.is_none() {
-                                    EVENT_AGGREGATOR.send(UiCommand::Serial(
-                                        SerialCommand::Keyboard(raw_input.to_string()),
-                                    ));
-                                }
-                            }
-                        }
-                        self.prev_dead_key = next_dead_key;
-                    }
-                }
-
-                // Regardless of whether this was a valid keyboard input or not, rest ignoring and
-                // whatever event was queued.
-                self.ignore_input_this_frame = false;
-                self.queued_input_events.clear();
+                EVENT_AGGREGATOR.send(UiCommand::Serial(SerialCommand::Keyboard(
+                    string.to_string(),
+                )));
             }
             _ => {}
         }
-    }
-
-    fn should_ignore_input(&self) -> bool {
-        let settings = SETTINGS.get::<KeyboardSettings>();
-        self.ignore_input_this_frame || (self.logo && !settings.use_logo)
     }
 
     fn maybe_get_keybinding(&self, key_event: &KeyEvent) -> Option<String> {
